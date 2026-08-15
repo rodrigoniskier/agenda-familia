179fad2d52dd69815ea3cef01e4a46a538c7ab12
dev.Rodrigo Niskier <niskier.rodrigo@gmail.com>
2026-08-15 20:32:09 -0300

    feat: enable mobile navigation shell

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import AppRealtime from './AppRealtime.tsx';
+import { MobileBottomNav } from './components/MobileBottomNav.tsx';
 import './index.css';
+import './mobile-enhancements.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppRealtime />
+    <MobileBottomNav />
   </StrictMode>,
 );
