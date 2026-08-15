c9d47fb3c7278b68062cebbd9b050a89d818987a
dev.Rodrigo Niskier <niskier.rodrigo@gmail.com>
2026-08-15 20:48:12 -0300

    Integra lembretes diários ao layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import AppRealtime from './AppRealtime.tsx';
+import { DailyCheckinsPortal } from './components/DailyCheckinsPortal.tsx';
 import { MobileBottomNav } from './components/MobileBottomNav.tsx';
 import './index.css';
 import './mobile-enhancements.css';
@@ -8,6 +9,7 @@ import './mobile-enhancements.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AppRealtime />
+    <DailyCheckinsPortal />
     <MobileBottomNav />
   </StrictMode>,
 );
